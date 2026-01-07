b008701e605e1061bf174aae6441fee0098f6573
LittFlower <xzy1476569428@163.com>
2026-01-07 15:33:18 +0800

    fix: import sys for exit in exe.

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ from UserLogin import userlogin
 from GetMajorid import fetch_major_id
 from PostClass import postclass
 from teacherEvaluate import teacherEvaluate
-import sys
+from sys import argv, exit
 
 # staticINF.print_log("Hint: If you want to start the feature to automaticly evaluate teachers, you should pass in the parameter E.", "INFO")
 
